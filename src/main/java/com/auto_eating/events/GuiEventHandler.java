7b156f5549a488e2e567f369d6900ffd69168000
GingerYJ <1084905530@qq.com>
2026-06-29 14:41:02 +0800

    Draw auto-eat slot frame over items

--- src/main/java/com/auto_eating/events/GuiEventHandler.java
+++ src/main/java/com/auto_eating/events/GuiEventHandler.java
@@ -16,8 +16,11 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 @SideOnly(Side.CLIENT)
 public class GuiEventHandler {
 
-    private static final ResourceLocation SLOT_TEX = new ResourceLocation("textures/gui/container/generic_54.png");
     private static final ResourceLocation FOOD_SLOT_TEX = new ResourceLocation("auto_eating", "textures/gui/emptyslots/emptyslot_food.png");
+
+    private static final int SLOT_BACKGROUND = 0xFF8B8B8B;
+    private static final int SLOT_SHADOW = 0xFF373737;
+    private static final int SLOT_HIGHLIGHT = 0xFFFFFFFF;
 
     @SubscribeEvent
     public void onDrawForeground(GuiContainerEvent.DrawForeground event) {
@@ -32,16 +35,26 @@ public class GuiEventHandler {
         GlStateManager.color(1F, 1F, 1F, 1F);
 
         for (Slot slot : gui.inventorySlots.inventorySlots) {
-            if (slot instanceof SlotAutoFood && !slot.getHasStack()) {
-                mc.getTextureManager().bindTexture(SLOT_TEX);
-                Gui.drawModalRectWithCustomSizedTexture(slot.xPos - 1, slot.yPos - 1, 7, 17, 18, 18, 256, 256);
-                mc.getTextureManager().bindTexture(FOOD_SLOT_TEX);
-                Gui.drawModalRectWithCustomSizedTexture(slot.xPos + 1, slot.yPos + 1, 0, 0, 16, 16, 16, 16);
+            if (slot instanceof SlotAutoFood) {
+                if (!slot.getHasStack()) {
+                    Gui.drawRect(slot.xPos, slot.yPos, slot.xPos + 16, slot.yPos + 16, SLOT_BACKGROUND);
+                    mc.getTextureManager().bindTexture(FOOD_SLOT_TEX);
+                    Gui.drawModalRectWithCustomSizedTexture(slot.xPos, slot.yPos, 0, 0, 16, 16, 16, 16);
+                }
+
+                drawSlotFrame(slot.xPos, slot.yPos);
             }
         }
 
         GlStateManager.disableBlend();
         GlStateManager.enableDepth();
         GlStateManager.enableLighting();
+    }
+
+    private static void drawSlotFrame(int x, int y) {
+        Gui.drawRect(x - 1, y - 1, x + 17, y, SLOT_SHADOW);
+        Gui.drawRect(x - 1, y - 1, x, y + 17, SLOT_SHADOW);
+        Gui.drawRect(x - 1, y + 16, x + 17, y + 17, SLOT_HIGHLIGHT);
+        Gui.drawRect(x + 16, y - 1, x + 17, y + 17, SLOT_HIGHLIGHT);
     }
 }
